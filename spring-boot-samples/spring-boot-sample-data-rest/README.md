92d141cf42ef2f5fce976e04a0a811e2f7b414a5
Olav Loite <koloite@gmail.com>
2017-12-17 13:36:26 +0100

    created sample application for spring-boot-data-rest and Google Cloud Spanner

---
+++ spring-boot-samples/spring-boot-sample-data-rest/README.md
@@ -0,0 +1,8 @@
+# spring-boot-sample-data-rest with Google Cloud Spanner
+Sample application for Spring Boot Data REST in combination with Google Cloud Spanner
+
+This sample application shows how you could use Spring Boot, REST, JPA and Hibernate in combination with Google Cloud Spanner. The most important modifications from the original sample application are:
+ * Specify Cloud Spanner JDBC driver and data source URL
+ * Include Cloud Spanner Hibernate dialect
+ * Change the primary key columns of the entities from using sequences to using client side generated id's based on UUID's
+  
--- spring-boot-samples/spring-boot-sample-data-rest/src/main/java/sample/data/rest/domain/Hotel.java
+++ spring-boot-samples/spring-boot-sample-data-rest/src/main/java/sample/data/rest/domain/Hotel.java
@@ -16,26 +16,16 @@
 
 package sample.data.rest.domain;
 
-import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
 
 import org.hibernate.annotations.NaturalId;
 
 @Entity
-public class Hotel implements Serializable {
-
+public class Hotel extends BaseEntity
+{
 	private static final long serialVersionUID = 1L;
-
-	@Id
-	@SequenceGenerator(name = "hotel_generator", sequenceName = "hotel_sequence", initialValue = 28)
-	@GeneratedValue(generator = "hotel_generator")
-	private Long id;
 
 	@ManyToOne(optional = false)
 	@NaturalId
@@ -51,27 +41,33 @@ public class Hotel implements Serializable {
 	@Column(nullable = false)
 	private String zip;
 
-	protected Hotel() {
+	protected Hotel()
+	{
 	}
 
-	public Hotel(City city, String name) {
+	public Hotel(City city, String name)
+	{
 		this.city = city;
 		this.name = name;
 	}
 
-	public City getCity() {
+	public City getCity()
+	{
 		return this.city;
 	}
 
-	public String getName() {
+	public String getName()
+	{
 		return this.name;
 	}
 
-	public String getAddress() {
+	public String getAddress()
+	{
 		return this.address;
 	}
 
-	public String getZip() {
+	public String getZip()
+	{
 		return this.zip;
 	}
 }
