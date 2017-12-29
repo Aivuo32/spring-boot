cecc09b2dc91e67a6d64375f4426dcbb1f33e50b
Olav Loite <koloite@gmail.com>
2017-12-29 19:44:14 +0100

    flyway sample adapted to work with Google Cloud Spanner

--- spring-boot-samples/spring-boot-sample-flyway/src/main/java/sample/flyway/Person.java
+++ spring-boot-samples/spring-boot-sample-flyway/src/main/java/sample/flyway/Person.java
@@ -17,38 +17,38 @@
 package sample.flyway;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 
 @Entity
-public class Person {
-	@Id
-	@SequenceGenerator(name = "person_generator", sequenceName = "person_sequence", allocationSize = 1)
-	@GeneratedValue(generator = "person_generator")
-	private Long id;
+public class Person extends BaseEntity
+{
+	private static final long serialVersionUID = 1L;
+
 	private String firstName;
 	private String lastName;
 
-	public String getFirstName() {
+	public String getFirstName()
+	{
 		return this.firstName;
 	}
 
-	public void setFirstName(String firstName) {
+	public void setFirstName(String firstName)
+	{
 		this.firstName = firstName;
 	}
 
-	public String getLastName() {
+	public String getLastName()
+	{
 		return this.lastName;
 	}
 
-	public void setLastName(String lastname) {
+	public void setLastName(String lastname)
+	{
 		this.lastName = lastname;
 	}
 
 	@Override
-	public String toString() {
-		return "Person [firstName=" + this.firstName + ", lastName=" + this.lastName
-				+ "]";
+	public String toString()
+	{
+		return "Person [firstName=" + this.firstName + ", lastName=" + this.lastName + "]";
 	}
 }
