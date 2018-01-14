5a7c3e98b343258a690c0a55aadf69d9a453f4c8
Olav Loite <koloite@gmail.com>
2018-01-14 13:24:19 +0100

    downgraded to release version and updated hibernate dialect to 0.5

--- spring-boot-samples/spring-boot-sample-data-rest/src/test/java/sample/data/rest/service/CityRepositoryIntegrationTests.java
+++ spring-boot-samples/spring-boot-sample-data-rest/src/test/java/sample/data/rest/service/CityRepositoryIntegrationTests.java
@@ -16,17 +16,17 @@
 
 package sample.data.rest.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import sample.data.rest.domain.City;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import sample.data.rest.domain.City;
 
 /**
  * Integration tests for {@link CityRepository}.
@@ -36,31 +36,33 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CityRepositoryIntegrationTests {
+public class CityRepositoryIntegrationTests
+{
 
 	@Autowired
 	CityRepository repository;
 
 	@Test
-	public void findsFirstPageOfCities() {
+	public void findsFirstPageOfCities()
+	{
 
-		Page<City> cities = this.repository.findAll(PageRequest.of(0, 10));
+		Page<City> cities = this.repository.findAll(new PageRequest(0, 10));
 		assertThat(cities.getTotalElements()).isGreaterThan(20L);
 	}
 
 	@Test
-	public void findByNameAndCountry() {
-		City city = this.repository.findByNameAndCountryAllIgnoringCase("Melbourne",
-				"Australia");
+	public void findByNameAndCountry()
+	{
+		City city = this.repository.findByNameAndCountryAllIgnoringCase("Melbourne", "Australia");
 		assertThat(city).isNotNull();
 		assertThat(city.getName()).isEqualTo("Melbourne");
 	}
 
 	@Test
-	public void findContaining() {
-		Page<City> cities = this.repository
-				.findByNameContainingAndCountryContainingAllIgnoringCase("", "UK",
-						PageRequest.of(0, 10));
+	public void findContaining()
+	{
+		Page<City> cities = this.repository.findByNameContainingAndCountryContainingAllIgnoringCase("", "UK",
+				new PageRequest(0, 10));
 		assertThat(cities.getTotalElements()).isEqualTo(3L);
 	}
 }
